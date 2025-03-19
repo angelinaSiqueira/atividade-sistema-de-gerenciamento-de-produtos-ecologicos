0035d9e103f764ec36c4da2af503926025c1d365
unknown <alexiaprangegoncalves.trabalho@gmail.com>
2025-03-19 08:53:31 -0300

    Primeiro commit

---
+++ atividade-sala/index.ts
@@ -0,0 +1,22 @@
+let nome : string = "Alexia";
+
+console.log(nome)
+
+var pi = 3.14;
+
+const idade: number = 16;
+
+function saudacao(nome, idade){
+
+    console.log("Eu me chamo ".concat(nome, " e tenho ".concat(idade, "anos") ));
+}
+
+var pessoa = {
+    nome: "Aline",
+    idade: 11,
+    cidade: "Salvador",
+    apresentar: function(){
+        return "O meu nome"
+    }
+
+}
