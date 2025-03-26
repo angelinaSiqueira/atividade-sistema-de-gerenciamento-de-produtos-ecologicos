a85ffedf25c03287596ed4d38a70b5365e33255f
ianfe <ianfelipe03082007@gmail.com>
2025-03-26 08:42:05 -0300

    commit-1

--- atividade_casa/item_decoracao_sustentavel.ts
+++ atividade_casa/item_decoracao_sustentavel.ts
@@ -32,4 +32,20 @@ class item_decoracao_sustentavel implements produto_ecologico {
     set_preco(preco1: number): void {
         this.preco = preco1;
     }
+
+    get_material(): string {
+        return this.material
+    }
+
+    set_material(material1: string): void {
+        this.material = material1;
+    }
+
+    get_dimensoes() {
+        return this.dimensoes
+    }
+
+    set_dimensoes(dimensoes1: { largura: number, altura: number, profundidade: number }) {
+        this.dimensoes = dimensoes1;
+    }
 }
