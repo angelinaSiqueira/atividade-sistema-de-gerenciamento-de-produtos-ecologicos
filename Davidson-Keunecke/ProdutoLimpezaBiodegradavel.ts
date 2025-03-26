af0ec6433811d1a1648d1a8080b4b2ae1f4f553b
Davidosom <davidsonkeunecke@gmail.com>
2025-03-26 08:50:45 -0300

    atividade-sistema-de-gerenciamento-de-produtos-ecologicos-Davidson

---
+++ Davidson-Keunecke/ProdutoLimpezaBiodegradavel.ts
@@ -0,0 +1,12 @@
+import { ProdutoEcologico } from "./ProdutoEcologico"
+
+class ProdutoLimpezaBiodegradavel implements ProdutoEcologico{
+    nome: string
+    preco: number
+    volume: number
+    constructor(nome: string, preco: number, volume: number){
+        this.nome = nome
+        this.preco =  preco
+        this.volume =  volume
+    }
+}
