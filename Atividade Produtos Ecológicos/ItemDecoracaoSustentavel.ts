a2905c5fcf5f4cd9a9d3b5a597a426943afd6785
unknown <gabrielodeli8@gmail.com>
2025-03-27 09:01:30 -0300

    Projeto terminado

---
+++ Atividade Produtos Ecológicos/ItemDecoracaoSustentavel.ts
@@ -0,0 +1,23 @@
+import {ProdutoEcologico} from "./ProdutoEcologico";
+
+class ItemDecoracaoSustentavel implements ProdutoEcologico {
+  material: string;
+  nome: string;
+  preco: number;
+  dimensoes: { largura: number, altura: number, profundidade: number};
+
+  constructor (material: string, nome: string, preco: number, dimensoes: { largura: number, altura: number, profundidade: number}) {
+    this.material = material;
+    this.nome = nome;
+    this.preco = preco;
+    this.dimensoes = dimensoes;
+  }
+
+  exibir() {
+    console.log("Nome: " + this.nome + " / preço: " + this.preco + " / Material: " + this.material + " / dimensão: " + this.dimensoes)
+  }
+
+  cadastrar():void{
+    console.log("Foi cadastrado um Item de Decoracao Sustentavel" + this.nome)
+  }
+}
