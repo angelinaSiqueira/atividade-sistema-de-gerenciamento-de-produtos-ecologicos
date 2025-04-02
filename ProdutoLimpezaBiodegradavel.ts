8f9286874d614aedba7a8b7b7f144e14aae796bf
unknown <eduardadasilvam0@gmail.com>
2025-04-02 08:02:46 -0300

    atividade

---
+++ ProdutoLimpezaBiodegradavel.ts
@@ -0,0 +1,22 @@
+class ProdutoLimpezaBiodegradavel implements ProdutoEcologico{
+    nome:String;
+    preco:number;
+    volume:number;
+
+    constructor(nome:String, preco:number, volume:number){
+        this.nome=nome;
+        this.preco=preco;
+        this.volume= volume;
+    }
+    exibir(){
+        console.log("nome:"+ this.nome + "preco:" + this.preco + "volume:" + this.volume)
+    }
+    cadastrar():void{
+        console.log("volume:" + this.volume)
+    }
+    }
+    const produto = new ProdutoLimpezaBiodegradavel("sabão", 15, 500);
+
+
+produto.exibir();
+produto.cadastrar();
